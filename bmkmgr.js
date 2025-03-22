edf39a6f8ac2c5001f5e735a7f764b9d646645ae
nliaquin <nickolasiaquinta@gmail.com>
2025-03-22 13:28:55 -0400

    feature: Add saveBookmarks function
    A function, saveBookmarks, has been implemented to allow persistence to
    bookmarks.json file.

--- bmkmgr.js
+++ bmkmgr.js
@@ -7,4 +7,8 @@ function loadBookmarks(filePath = "bookmarks.json") {
     return JSON.parse(fs.readFileSync(filePath, "utf8"));
 }
 
-module.exports = { loadBookmarks };
+function saveBookmarks(bookmarks, filePath = "bookmarks.json") {
+    fs.writeFileSync(filePath, JSON.stringify(bookmarks, null, 4), "utf8");
+}
+
+module.exports = { loadBookmarks, saveBookmarks };
