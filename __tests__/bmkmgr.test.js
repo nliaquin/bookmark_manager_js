b3863eff6a005fc5e8fee3f938121cfa15c556cd
nliaquin <nickolasiaquinta@gmail.com>
2025-03-22 21:09:25 -0400

    test: Modify test for deleteBookmark function
    Modified  __test__/bmkmgr.test.js to test deleteBookmark function

--- __tests__/bmkmgr.test.js
+++ __tests__/bmkmgr.test.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const { loadBookmarks, saveBookmarks, addBookmark, printBookmarks, updateBookmark } = require("../bmkmgr");
+const { loadBookmarks, saveBookmarks, addBookmark, printBookmarks, updateBookmark, deleteBookmark } = require("../bmkmgr");
 
 const TEST_FILE_PATH = path.join(__dirname, "../test-data/bookmarks.json");
 /*
@@ -9,17 +9,32 @@ afterAll(() => {
 });
 */
 
-test("updateBookmark modified the user-specified field of a given bookmark by name", () => {
-    const initialBookmark = [
-        { name: "Outlook", hyperlink: "https://outlook.com", category: "Email" }
-    ];
+test("deleteBookmark removes a given bookmark by name", () => {
+    const mockData = [
+        { name: "Google", hyperlink: "https://google.com", category: "Search Engines" },
+        { name: "DuckDuckGo", hyperlink: "https://duckduckgo.com", category: "Search Engines" },
+        { name: "Outlook", hyperlink: "https://outlook.com", category: "Microsoft 365" },
+        { name: "Office", hyperlink: "https://office.com", category: "Microsoft 365" },
+        { name: "Facebook", hyperlink: "https://facebook.com", category: "Social Media" }
+    ]
 
-    saveBookmarks(initialBookmark, TEST_FILE_PATH);
+    saveBookmarks(mockData, TEST_FILE_PATH);
 
-    const updatedBookmark = updateBookmark("Outlook", "category", "Microsoft 365", TEST_FILE_PATH);
-    expect(updatedBookmark).toBe(true);
+    const deleted = deleteBookmark("Facebook", TEST_FILE_PATH);
+    expect(deleted).toBe(true);
 
-    const result = loadBookmarks(TEST_FILE_PATH);
-    expect(result[0].category).toBe("Microsoft 365");
+    const bookmarks = loadBookmarks(TEST_FILE_PATH);
+    expect(bookmarks).toEqual([
+        { name: "Google", hyperlink: "https://google.com", category: "Search Engines" },
+        { name: "DuckDuckGo", hyperlink: "https://duckduckgo.com", category: "Search Engines" },
+        { name: "Outlook", hyperlink: "https://outlook.com", category: "Microsoft 365" },
+        { name: "Office", hyperlink: "https://office.com", category: "Microsoft 365" },
+    ]);
+});
+
+test("deleteBookmark returns false if bookmark not found", () => {
+    saveBookmarks([], TEST_FILE_PATH);
+    const result = deleteBookmark("Reddit", TEST_FILE_PATH);
+    expect(result).toBe(false);
 });
 
